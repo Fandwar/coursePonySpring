7e5273b2a66491d2e6c1d301ccf30d6bb58209c7
Your Name <you@example.com>
2018-11-07 13:59:21 +0100

    Ajout des crossOrigins pour la liason vers Angular

--- src/main/java/fr/dta/nicolas/tp/coursepony/controller/CourseController.java
+++ src/main/java/fr/dta/nicolas/tp/coursepony/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ public class CourseController {
 	@Autowired
 	CourseDAO courseDAO;
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping("/")
 	public List<Course> getAll() {
 		return courseDAO.findAll();
@@ -44,7 +46,7 @@ public class CourseController {
 		courseDAO.deleteById(id);
 	}
 	
-	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/")
 	public @Valid Course save(@RequestBody @Valid Course course ) {
 		return courseDAO.save(course);
--- src/main/java/fr/dta/nicolas/tp/coursepony/controller/PonyController.java
+++ src/main/java/fr/dta/nicolas/tp/coursepony/controller/PonyController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ public class PonyController {
 	@Autowired
 	PonyDAO ponyDAO;
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping("/")
 	public List<Pony> getAll() {
 		return ponyDAO.findAll();
@@ -46,7 +48,7 @@ public class PonyController {
 		ponyDAO.deleteById(id);
 	}
 	
-	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/")  
 	public Pony save(@RequestBody @Valid Pony pony ) {
 		return ponyDAO.save(pony);
